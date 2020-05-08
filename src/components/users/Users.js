7c3d0e50818367086ac319d9a00e6e05e22bd46c
TutorialSpiller <mdjonesonly310@gmail.com>
2020-05-08 02:03:20 -0700

    formatter update

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -5,13 +5,13 @@ class Users extends Component {
   state = {
     users: [
       {
-        id: "1,",
+        id: "1",
         login: "mojombo",
         avatar_url: "https://avatars0.githubusercontent.com/u/1?v=4",
         html_url: "https://github.com/mojombo",
       },
       {
-        id: "2,",
+        id: "2",
         login: "defunkt",
         avatar_url: "https://avatars0.githubusercontent.com/u/2?v=4",
         html_url: "https://github.com/defunkt",

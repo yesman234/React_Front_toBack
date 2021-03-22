6bd97907f7206caaa1d83941011e6d7a89902c36
Marcellus Devon Jones <mdjonesonly310@gmail.com>
2021-03-22 12:53:35 -0700

    state level search bar start

---
+++ src/components/layout/Spinner.js
@@ -0,0 +1,7 @@
+import React, {Fragment} from 'react'
+
+export const Spinner = () => 
+        <Fragment>
+            <img src={spinner} alt="Loading..." style={{width: '500px', margin:'auto', display:'block'}} />
+        </Fragment>
+    
---
+++ src/components/users/Search.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react'
+
+export class Search extends Component {
+    state={
+       text:'' 
+    }
+
+    onSubmit=(e)=>{
+        
+        this.props.searchUsers(this.state.text);
+        this.setState({text:''})
+    }
+    onChange = e =>
+        this.setState({text: e.target.value})
+    
+    render() {
+        return (
+            <div>
+                <form onSubmit={this.onSubmit.bind(this)} className='form'>
+                    <input type='text' name ="text" placeholder='Search Users....' value={this.state.text} onChange={this.onChange}/>
+                    <input type='submit' value ='Search' className="btn btn-dark btn-block" />
+                </form>
+            </div>
+        )
+    }
+}
+
+export default Search
